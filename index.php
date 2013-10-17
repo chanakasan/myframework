45e36ec0cbda72cc81d64fd2900cb21425f04441
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-10-18 03:05:35 +0530

    add HttpFoundation

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
+<?php 
+// framework/index.php
  
-$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+require_once __DIR__.'/autoload.php';
  
-header('Content-Type: text/html; charset=utf-8');
-printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+ 
+$request = Request::createFromGlobals();
+ 
+$input = $request->get('name', 'World');
+ 
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+ 
+$response->send();
