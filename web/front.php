4f12e92f16664ee8818d54a0461899fdac61e89b
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-10-18 17:09:58 +0530

    Add routing component

--- web/front.php
+++ web/front.php
@@ -1,26 +1,30 @@
 <?php
 // framework/front.php
  
-require_once __DIR__.'/../src/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
  
 $request = Request::createFromGlobals();
-$response = new Response();
+$routes = include __DIR__.'/../src/app.php';
  
-$map = array(
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-);
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
  
-$path = $request->getPathInfo();
-if (isset($map[$path])) {
-	ob_start();
-    require sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
-    $response->setContent(ob_get_clean());
-} else {
+try {
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+ 
+    $response = new Response(ob_get_clean());
+} catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
+} catch (Exception $e) {
+    $response = new Response('An error occurred', 500);
 }
  
 $response->send();
+ 
