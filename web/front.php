055d0b9d9d2848cb9119f763345c7c8555a042b0
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-10-18 16:54:11 +0530

    update map

--- web/front.php
+++ web/front.php
@@ -10,18 +10,17 @@ $request = Request::createFromGlobals();
 $response = new Response();
  
 $map = array(
-    '/hello' => __DIR__.'/../src/pages/hello.php',
-    '/bye'   => __DIR__.'/../src/pages/bye.php',
+    '/hello' => 'hello',
+    '/bye'   => 'bye',
 );
  
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
 	ob_start();
-    require $map[$path];
+    require sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
     $response->setContent(ob_get_clean());
 } else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+    $response = new Response('Not Found', 404);
 }
  
 $response->send();
